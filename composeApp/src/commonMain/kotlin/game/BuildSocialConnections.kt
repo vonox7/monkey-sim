a4f348fdfb5d209b699b475ac0579f62a474c69a
Filippo Orru <filippoorru@hotmail.com>
2024-04-20 22:44:14 +0200

    Start dating

--- composeApp/src/commonMain/kotlin/game/BuildSocialConnections.kt
+++ composeApp/src/commonMain/kotlin/game/BuildSocialConnections.kt
@@ -15,16 +15,26 @@ fun Game.buildSocialConnections(elapsedHours: Double) {
     }
 }
 
-private fun Game.buildSocialConnections(actors: List<Actor>, elapsedHours: Double) {
+
+private fun buildSocialConnections(actors: List<Actor>, elapsedHours: Double) {
   actors.forEach { actor ->
     if (actor.perceivedState.socializeFactor > Random.nextDouble() * elapsedHours) {
       val randomOtherActor = actors.random().takeIf { it != actor } ?: return@forEach
+
       actor.socializeWith(randomOtherActor)
       randomOtherActor.socializeWith(actor)
     }
   }
 }
 
+private const val datingThreshold = 10
 private fun Actor.socializeWith(other: Actor) {
-  this.socialConnections.connections[other] = (this.socialConnections.connections[other] ?: 0.0) + 1
+  val connection = social.connections[other]
+  social.connections[other] = (connection ?: 0.0) + 1
+
+  if (connection != null && connection > datingThreshold) {
+    social.connections.remove(other)
+    social.connections.remove(this)
+    social.partner = other
+  }
 }
