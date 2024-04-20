cb562abb9c59308872b218ea8741b7429d5793c3
Filippo Orru <filippoorru@hotmail.com>
2024-04-20 11:49:45 +0200

    Maximise window

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,17 @@
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "monkey-sim") {
-        App()
-    }
+  val state = rememberWindowState(
+    placement = WindowPlacement.Maximized,
+  )
+  Window(
+    onCloseRequest = ::exitApplication,
+    title = "monkey-sim",
+    state = state,
+  ) {
+    App()
+  }
 }
