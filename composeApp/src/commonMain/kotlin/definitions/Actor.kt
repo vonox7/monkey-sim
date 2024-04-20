a47f7e589b8cd15d1ac1bb46a97a33365aa40a1a
Filippo Orru <filippoorru@hotmail.com>
2024-04-20 11:08:32 +0200

    Actor.randomInWorld

--- composeApp/src/commonMain/kotlin/definitions/Actor.kt
+++ composeApp/src/commonMain/kotlin/definitions/Actor.kt
@@ -1,5 +1,7 @@
 package definitions
 
+import kotlin.random.Random
+
 class Actor(
   val name: String,
   val needs: Needs,
@@ -7,11 +9,42 @@ class Actor(
   val yearsOfEducation: Int,
   val age: Int,
   val sex: Sex,
-  val socialConnections: SocialConnections,
   var currentPosition: Position,
   var home: Home,
+  var work: Work? = null,
   // TODO partner
-)
+) {
+  val socialConnections: SocialConnections = SocialConnections()
+
+  companion object {
+    private val firstNames = listOf(
+      "Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Judy", "Kevin", "Linda", "Mallory", "Niaj", "Oscar",
+      "Peggy", "Quentin", "Rene", "Steve", "Trent", "Ursula", "Victor", "Walter", "Xavier", "Yvonne", "Zelda"
+    )
+
+    private val lastNames = listOf(
+      "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson",
+      "White", "Harris", "Martin", "Thompson", "Garcia", "Martinez", "Robinson", "Clark", "Rodriguez", "Lewis", "Lee", "Walker", "Hall",
+    )
+
+    fun randomInWorld(worldWidth: Int, worldHeight: Int, home: Home, work: Work?): Actor {
+      return Actor(
+        name = "${firstNames.random()} ${lastNames.random()}",
+        sex = when (Random.nextFloat()) {
+          in 0.0..0.45 -> Sex.Male
+          in 0.4..0.9 -> Sex.Female
+          else -> Sex.Other
+        },
+        needs = Needs.default(),
+        yearsOfEducation = Random.nextInt(0, 20),
+        age = Random.nextInt(0, 80),
+        currentPosition = Position(Random.nextInt(0, worldWidth), Random.nextInt(0, worldHeight)),
+        home = home,
+        work = work,
+      )
+    }
+  }
+}
 
 enum class Sex {
   Male, Female, Other;
