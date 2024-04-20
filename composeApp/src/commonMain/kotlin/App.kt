a4f99bfde2743bbcc7952606a81a4f126a723d73
Filippo Orru <filippoorru@hotmail.com>
2024-04-20 14:25:33 +0200

    Fix launch every frame lol

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -3,13 +3,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
@@ -26,12 +25,12 @@ fun App() {
     val lastTickDuration = remember { mutableStateOf(Duration.ZERO) }
     val maxTickDuration = remember { mutableStateOf(Duration.ZERO) }
 
-    val scope = rememberCoroutineScope()
-    scope.launch {
+    LaunchedEffect(key1 = game) {
+      println("Launching")
       var lastTime = TimeSource.Monotonic.markNow()
       while (true) {
         val renderTime = measureTime {
-          game.tick(elapsedHours = lastTime.elapsedNow().inWholeMilliseconds / 300_000.0)
+          game.tick(elapsedHours = lastTime.elapsedNow().inWholeMilliseconds / 300.0)
           tick.value += 1
           lastTime = TimeSource.Monotonic.markNow()
         }
@@ -54,8 +53,8 @@ fun App() {
       }
       Box(
         modifier = Modifier
-          .weight(1f)
-          .aspectRatio(1f)
+            .weight(1f)
+            .aspectRatio(1f)
       ) {
         Canvas(modifier = Modifier.fillMaxSize()) {
           CanvasDrawer(this, game.world.width, game.world.height).draw(game.world)
