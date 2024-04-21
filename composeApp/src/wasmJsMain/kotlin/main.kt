2eca4de198bc2adb6f53c2288254cda0acc10baf
Valentin Slawicek <valentin.slawi@gmail.com>
2024-04-21 20:04:29 +0200

    Loading spinner for wasm

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,7 +1,9 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
+import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    document.querySelector(".lds-ring")?.remove()
     CanvasBasedWindow(canvasElementId = "ComposeTarget") { App() }
 }
